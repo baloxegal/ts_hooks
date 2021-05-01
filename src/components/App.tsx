b9ddd4ebc8589c63b1ff7080922e5e66f24c3aaf
baloxegal <baloxegal@gmail.com>
2021-05-01 20:24:02 +0300

    +-+-+-+-+-

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import '../App.css';
+import Header from './Header';
+import Body from './Body';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header text="Cel mai bun curs valutar din Moldova!!!"/>
+      <Body/>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</npm code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Body.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from './SignIn';
+
+const Body = () => {
+    
+    return (
+        <h2 className="Goodness">            
+            <SignIn/>            
+        </h2>        
+    )
+}
+
+export default Body
