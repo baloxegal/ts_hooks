1359f547ff7c7633a4ea14fd49031f11c91030c7
baloxegal <baloxegal@gmail.com>
2021-06-16 16:49:14 +0300

    +-+-/***

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     return (
         <div className="App-Home">            
             
-            <h1>WE ARE HEAR</h1>
+            <h1>WE ARE HERE</h1>
             <h1>JOIN US</h1>
                         
         </div>        
